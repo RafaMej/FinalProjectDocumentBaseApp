6c1e4fc0170fa3d5e23a4e1702db85634960c99c
Rafa <rafaelmejia2005@gmail.com>
2026-05-11 21:04:43 -0600

    Initial Commit

---
+++ DocumentBase/DocumentBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocumentBaseApp.swift
+//  DocumentBase
+//
+//  Created by Rafael on 11/05/26.
+//
+
+import SwiftUI
+
+@main
+struct DocumentBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
